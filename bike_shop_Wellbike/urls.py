e905117f2a4b4df9961ad881cff9222374e0ef2f
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-04-06 21:33:39 +0300

    Try to fix adding css and js

--- bike_shop_Wellbike/urls.py
+++ bike_shop_Wellbike/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,10 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-]
-
-# To display images
-
+] \
+ \
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
