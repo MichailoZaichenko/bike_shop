a9a75ff3ccac0c03d3a7fd65a1d76df24373b368
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-04-06 21:31:05 +0300

    Try to fix adding css and js

--- bike_shop_Wellbike/urls.py
+++ bike_shop_Wellbike/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 ]
 
 # To display images
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
