3c96acd8340e1654e5b5e2cb8ec42316b9a773ca
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-06-08 01:17:27 +0300

    translated static text correctly, mod db paying way

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -72,8 +72,14 @@ WSGI_APPLICATION = 'bike_shop_Wellbike.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'mRBFp13xxf5SIRgsGZWJ',
+        'HOST': 'containers-us-west-83.railway.app',
+        'PORT': '6380',
     }
 }
 
