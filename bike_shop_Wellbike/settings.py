4a0c3394c461193802109a804a6144ea545651de
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-06-11 14:30:23 +0300

    add caches and postgressdb

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -69,18 +69,25 @@ WSGI_APPLICATION = 'bike_shop_Wellbike.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 #
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'mRBFp13xxf5SIRgsGZWJ',
+        'HOST': 'containers-us-west-83.railway.app',
+        'PORT': '6380',
     }
 }
-
-# import dj_database_url
-#
-# DATABASES = {
-#     'default': dj_database_url.parse('')
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
