65ab8019a9147bcff82fee8749da9a2d6c60d0ff
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-09-08 22:02:37 +0300

    add import export in admin.py form files

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -152,13 +152,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'michailo.zaichenko@gmail.com'
-EMAIL_HOST_PASSWORD = 'micha@_06'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'michailo.zaichenko@gmail.com'
+# EMAIL_HOST_PASSWORD = 'micha@_06'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
