b0a47923e378dc42c7fbfe2a6fea25dca55c6f8c
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-09-08 21:13:39 +0300

    add import export in admin.py form files

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bike_shop_Wellbike.wsgi.application'
+# WSGI_APPLICATION = 'bike_shop_Wellbike.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
